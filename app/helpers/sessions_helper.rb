e8d4a9e7e6295861d05991164cf75faa93fbd035
Bach Do <dohuybach714@gmail.com>
2020-03-03 16:30:14 +0700

    fix pull #7

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,7 +13,8 @@ module SessionsHelper
 
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
-        !current_user.nil?
+        #!current_user.nil?
+        current_user.present?
     end
 
     # Logs out the current user.
